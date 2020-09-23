197261f7be7d0f4d3d3fff5e9436a9ea2b822509
Alex Shorsher <alex.shorsher@gmail.com>
2020-09-22 23:22:06 -0400

    fix db import, add routing to front end
     - fixed incorrect DB path import
     - front end is in the works :)

--- backend/controllers/election.js
+++ backend/controllers/election.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongo = require('../db')
+const mongo = require('../db/mongo')
 
 const {
   FROM_ADDRESS,
@@ -9,6 +9,7 @@ let electionController = function (swaggerClient) {
     let router = express.Router();
 
     router.post('/api/election', async (req, res) => {
+        //TODO: add candidates to mongo
         // Note: we really only want to deploy a new instance of the contract
         //       when we are initializing our on-chain state for the first time.
         //       After that the application should keep track of the contract address.
@@ -58,12 +59,6 @@ let electionController = function (swaggerClient) {
       catch(err) {
         res.status(500).send({error: `${err.response && err.response.body && err.response.text}\n${err.stack}`});
       }
-    });
-
-    router.post('/api/election/candidates', async (req, res) => {
-      const { db } = mongo;
-      const electionColl = db.collection('election');
-
     });
 
     return router;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import Button from '@material-ui/core/Button'
+import './Home.css';
+
+export default function Home() {
+
+  const [loading, setLoading] = useState(false);
+  const [deployState, setDeployState] = useState("Deploy");
+  const [contractAddress, setContractAddress] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  async function deployContract() {
+    setLoading(true);
+    setErrorMsg(null);
+    setDeployState("Deploying...")
+    try {
+      const res = await fetch('/api/election', {
+        method: 'POST',
+        body: JSON.stringify({
+          candidateOne: "Candidate One",
+          candidateTwo: "Candidate Two"
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const {contractAddress : addr, error} = await res.json();
+      if (!res.ok) {
+        setErrorMsg(error)
+        setDeployState("Error! - Retry Deploy");
+      } else {
+        setContractAddress(addr);
+        setDeployState("Redeploy");
+      }
+    } catch (err) {
+      setErrorMsg(err.stack)
+      setDeployState("Error! - Retry Deploy");
+    }
+    setLoading(false);
+  }
+
+  async function vote(candidate) {
+    setLoading(true);
+    setErrorMsg(null);
+
+    try {
+      const res = await fetch(`/api/election/${contractAddress}/vote`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          candidate: candidate
+        })
+      });
+      const {error} = await res.json();
+      if (!res.ok) {
+        setErrorMsg(error);
+      }
+    } catch (err) {
+      setErrorMsg(err.stack);
+    }
+
+    setLoading(false);
+  }
+
+  async function getResults() {
+    setLoading(true);
+    setErrorMsg(null);
+
+     try {
+       const res = await fetch(`/api/election/${contractAddress}/results`);
+       const {results, error} = await res.json();
+       if (!res.ok) {
+         setErrorMsg(error);
+       } else {
+        console.log(results)
+       }
+     } catch (err) {
+       setErrorMsg(err.stack);
+     }
+    setLoading(false);
+  }
+
+return (
+  <div className="App">
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" aria-busy={loading}/>
+    <p>
+      <button type="button" className="App-button" disabled={loading} onClick={deployContract}>{deployState} Election</button>
+    </p>
+    { contractAddress && <p>
+      Contract Address: {contractAddress}
+    </p>}
+    <p>
+      <button type="button" className="App-button" onClick={() => vote(0)}>Candidate One</button>
+    </p>
+    <p>
+      <Button variant="contained" color="primary" onClick={() => vote(1)}>Candidate Two</Button>
+    </p>
+    <p>
+      <button type="button" className="App-button" onClick={getResults}>Get Results</button>
+    </p>
+    { errorMsg && <pre class="App-error">
+      Error: {errorMsg}
+    </pre>}
+    </header>
+  </div>
+);
+}
