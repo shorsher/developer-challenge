b66798aa6cec4026271238278b0aa9bcdd2438b0
Alex Shorsher <alex.shorsher@gmail.com>
2020-09-28 00:44:10 -0400

    homepage now uses makeStyles

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import './Home.css';
+import { makeStyles } from '@material-ui/core/styles';
 import ElectionCard from '../components/cards/ElectionCard';
 import VoteCard from '../components/cards/VoteCard';
 import ResultsCard from '../components/cards/ResultsCard';
 
-export default function Home() {
+// TODO: make this reusable
+const useStyles = makeStyles(() => ({
+  app: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection:' column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '40px',
+  }
+}));
 
-return (
-  <div className="App">
-    <div className="Card">
-      <ElectionCard />
+export default function Home() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.app}>
+      <div className={classes.card}>
+        <ElectionCard />
+      </div>
+      <div className={classes.card}>
+        <VoteCard />
+      </div>
+      <div className={classes.card}>
+        <ResultsCard />
+      </div>
     </div>
-    <div className="Card">
-      <VoteCard />
-    </div>
-    <div className="Card">
-      <ResultsCard />
-    </div>
-  </div>
-);
+  );
 }
